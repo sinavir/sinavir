359292e92ea3b2557179b3cc323abdc7fa79031c
sinavir <sinavir@sinavir.fr>
2023-09-30 11:52:00 +0200

    config: update syncthing options

--- config/shared/syncthing.nix
+++ config/shared/syncthing.nix
@@ -7,14 +7,15 @@ let
 in {
   services.syncthing = rec {
     enable = builtins.elem config.networking.hostName enabledHosts;
-    devices.sirius.id = "EAXHMJI-NNCZVVN-FHXRGAJ-VBSRP2K-HVVS75F-7AGJU26-ENW4I3G-FGDCMA6";
-    devices.polaris.id = "QMJEDQK-KMFJFB3-G7LID3Z-NXUHLES-4BGJCUJ-IKK4WXI-YN3GLG7-E7IGHAG";
-    # TODO add capella
-    folders = {
-      Notes = {
-        id = "75ujo-go7am";
-        devices = ["sirius" "polaris"];
-        path = "~/Notes";
+    settings = {
+      devices.sirius.id = "EAXHMJI-NNCZVVN-FHXRGAJ-VBSRP2K-HVVS75F-7AGJU26-ENW4I3G-FGDCMA6";
+      devices.polaris.id = "QMJEDQK-KMFJFB3-G7LID3Z-NXUHLES-4BGJCUJ-IKK4WXI-YN3GLG7-E7IGHAG";
+      folders = {
+        Notes = {
+          id = "75ujo-go7am";
+          devices = ["sirius" "polaris"];
+          path = "~/Notes";
+        };
       };
     };
   };
