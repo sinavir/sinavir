a33c2ee6c2365e0325e06c14f94e9e04ccd8d3dc
sinavir <sinavir@sinavir.fr>
2023-10-25 02:44:25 +0200

    config: add pixiecore on polaris

---
+++ config/machines/polaris/pixiecore.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, lib, nodes, ... }:
+let
+  bootSystem = nodes.nekkar.config;
+  netboot = pkgs.symlinkJoin {
+    name = "netboot";
+    paths = with bootSystem.config.system.build; [
+      netbootRamdisk
+      kernel
+      netbootIpxeScript
+    ];
+  };
+in {
+  services.pixiecore = {
+    enable = true;
+    kernel = "${netboot}/bzImage";
+    initrd = "${netboot}/initrd";
+    cmdLine = "init=${bootSystem.config.system.build.toplevel}/init loglevel=4";
+    debug = true;
+    dhcpNoBind = true;
+    port = 6666;
+    statusPort = 6666;
+  };
+}
