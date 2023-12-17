8b4d5727f331e8d87e18510a92e423f1c58d7e52
sinavir <sinavir@sinavir.fr>
2023-12-17 22:21:41 +0100

    nixexpr/castopod: update but still not working

--- nixexpr/castopod/castopod.nix
+++ nixexpr/castopod/castopod.nix
@@ -1,13 +1,13 @@
 { breakpointHook, php, fetchFromGitLab, lib }:
 php.buildComposerProject (finalAttrs: {
   pname = "castopod";
-  version = "1.6.5";
+  version = "1.7.2";
   src = fetchFromGitLab {
     domain = "code.castopod.org";
     owner = "adaures";
     repo = "castopod";
-    rev = "aeaee8ae64df485f7f0863e1947f06dafd5204cb";
-    hash = "sha256-RnJKczLL38By8HHTJ6fR1w0YuVTe2FL0F8pdX+jhigM=";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-7Tm/CtXWbLYbDWxSdk9vWMVxQ7fFF+nBSkRb2xJDsKw=";
   };
   composerStrictValidation = false;
   php = php.withExtensions ({ enabled, all }: with all; [
