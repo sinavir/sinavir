52b7677ece30d6fd3f7f0ecd894dbf6b3c9f6da0
sinavir <sinavir@sinavir.fr>
2024-03-10 10:34:48 +0100

    ragbv2: updated script

--- ragbv2/provisioning/script.py
+++ ragbv2/provisioning/script.py
@@ -1,27 +1,85 @@
+#!/nix/store/z46wvcd15152qnsry80p8ricxya2n2lr-python3-3.11.7-env/bin/python
+import sys
 import json
-import pprint
+import requests
+import logging
 
-import sseclient
+from colour import Color
+from pyjecteur.fixtures import Blinder, LedBar48Ch, Tradi
+from pyjecteur.lights import Universe
+from pyjecteur.widget import Widget
+
+if False:  # True:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+
+w = Widget("/dev/ttyUSB0")
+
+DIM = {
+    "blinder": 0.10,
+    "led_tub": 0.05,
+    "spot": 0.3,
+}
+
+u = Universe(w)
 
 
-def with_requests(url, headers):
-    """Get a streaming response for the given event feed using requests."""
-    import requests
-
-    return requests.get(url, stream=True, headers=headers)
-
-
-def with_urllib3(url, headers):
-    """Get a streaming response for the given event feed using urllib3."""
-    import urllib3
-
-    http = urllib3.PoolManager()
-    return http.request("GET", url, preload_content=False, headers=headers)
+def strToProj(s):
+    match s:
+        case "spot":
+            return Tradi()
+        case "led_tub":
+            return LedBar48Ch()
+        case "blinder":
+            return Blinder()
 
 
-url = "http://127.0.0.1:9999/api/sse"
-headers = {"Accept": "text/event-stream"}
-response = with_requests(url, headers)  # or with_requests(url, headers)
-client = sseclient.SSEClient(response)
-for event in client.events():
-    pprint.pprint(json.loads(event.data))
+r = requests.get("https://agb.hackens.org/api-docs/patch.json")
+patch = r.json()
+
+lights = {}
+
+update = {}
+
+current_addr = 0
+
+for k, v in patch["lights"].items():
+    lights[k] = strToProj(v["kind"])
+    u.register(lights[k], current_addr)
+    # update dmx since some params are set before
+    lights[k].update_dmx()
+    logging.info(
+        f"Light {k} of kind {v['kind']} is at DMX{current_addr+1} (PLS convention)"
+    )
+    for i, chan in enumerate(v["channels"]):
+        update[chan] = (k, i)  # put the light name
+    current_addr += lights[k].address_size
+
+
+def update_light(address, red, green, blue):
+    light, chan = update[address]
+    kind = patch["lights"][light]["kind"]
+    r, g, b = red * DIM[kind]/255, (green * DIM[kind])/255, (blue * DIM[kind])/255
+    match kind:
+        case "blinder":
+            lights[light].colors[chan] = Color(rgb=(r, g, b))
+        case "led_tub":
+            lights[light].colors[chan] = Color(rgb=(r, g, b))
+        case "spot":
+            lights[light].color = Color(rgb=(r, g, b))
+
+
+def run():
+    logging.info("Started")
+    for line in sys.stdin:
+        logging.debug(line)
+        if line.startswith("data:"):
+            dataStr = line[5:]
+            logging.debug(f"Received: {dataStr}")
+            data = json.loads(dataStr)
+            if data["address"] in update:
+                update_light(data["address"], **data["value"])
+
+run()
+
